0bad5f5c18ff595c0fba053853075b983f0c87ec
wing <zlcnkkm@21cn.com>
2015-11-07 05:12:44 +0800

    尝试使用TTF字体

--- wear/src/main/java/com/magicinstall/watchfacetemplet/InformationsDrawer.java
+++ wear/src/main/java/com/magicinstall/watchfacetemplet/InformationsDrawer.java
@@ -4,6 +4,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.content.res.Resources;
+import android.content.res.AssetManager;
 
 /**
  * Created by wing on 15/11/7.
@@ -12,9 +15,12 @@ public class InformationsDrawer extends WatchFaceDrawer{
     private Paint mTextPaint;
 
     public InformationsDrawer() {
+        Typeface mFace = Typeface.createFromAsset(Resources.getSystem().getAssets(), "fonts/04B08.TTF");
+
         mTextPaint = new Paint();
         mTextPaint.setColor(Color.RED);
         mTextPaint.setTextSize(8);
+        mTextPaint.setTypeface(mFace);
     }
 
     @Override
