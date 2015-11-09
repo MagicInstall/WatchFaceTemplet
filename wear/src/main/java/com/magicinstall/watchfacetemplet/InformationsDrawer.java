c7dd0958c98e3674cccacfa37a1a6d79d5f8fc86
wing <zlcnkkm@21cn.com>
2015-11-10 07:04:25 +0800

    加入使用安卓API 嘅传感器检测.

--- wear/src/main/java/com/magicinstall/watchfacetemplet/InformationsDrawer.java
+++ wear/src/main/java/com/magicinstall/watchfacetemplet/InformationsDrawer.java
@@ -1,16 +1,19 @@
 package com.magicinstall.watchfacetemplet;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.hardware.Sensor;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.Size;
 
 import java.text.DateFormat;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -31,9 +34,11 @@ public class InformationsDrawer extends WatchFaceDrawer{
 
     private Size mDisplaySize;
 
+    private String mSensorInfoString;
 
-    public InformationsDrawer(Resources resources) {
-        super(resources);
+
+    public InformationsDrawer(Resources resources, Context context) {
+        super(resources, context);
 //        mDrawString = ("1234567890\n" +
 //                "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n" +
 //                "abcdefghijklmnopqrstuvwxyz\n" +
@@ -66,6 +71,51 @@ public class InformationsDrawer extends WatchFaceDrawer{
 
         // 设置时间格式
         mDateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, Locale.getDefault());
+
+
+        //从传感器管理器中获得全部的传感器列表
+        List<Sensor> allSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        //打印传感器信息
+        System.out.println("有" + allSensors.size() + "个传感器:");
+        mSensorInfoString = allSensors.size() + "Sensors:\n";
+        for (Sensor s : allSensors) {
+            mSensorInfoString += s.getType() + s.getName() + "\n";
+
+//            String tempString = "\n" + "  设备名称：" + s.getName() + "\n" + "  设备版本：" + s.getVersion() + "\n" + "  供应商："
+//                    + s.getVendor() + "\n";
+//
+//            switch (s.getType()) {
+//                case Sensor.TYPE_ACCELEROMETER:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 加速度传感器accelerometer" + tempString);
+//                    break;
+//                case Sensor.TYPE_GYROSCOPE:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 陀螺仪传感器gyroscope" + tempString);
+//                    break;
+//                case Sensor.TYPE_LIGHT:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 环境光线传感器light" + tempString);
+//                    break;
+//                case Sensor.TYPE_MAGNETIC_FIELD:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 电磁场传感器magnetic field" + tempString);
+//                    break;
+//                case Sensor.TYPE_ORIENTATION:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 方向传感器orientation" + tempString);
+//                    break;
+//                case Sensor.TYPE_PRESSURE:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 压力传感器pressure" + tempString);
+//                    break;
+//                case Sensor.TYPE_PROXIMITY:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 距离传感器proximity" + tempString);
+//                    break;
+//                case Sensor.TYPE_TEMPERATURE:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 温度传感器temperature" + tempString);
+//                    break;
+//                default:
+//                    tx1.setText(tx1.getText().toString() + s.getType() + " 未知传感器" + tempString);
+//                    break;
+//            }
+        }
+
     }
 
     /*
@@ -94,7 +144,7 @@ public class InformationsDrawer extends WatchFaceDrawer{
                 str_visible +
                 str_ambient +
                 mDisplaySize.toString() + str_FPS +
-                "\n\n\n\n\n\n" +
+                mSensorInfoString +
                 mDateString,
                 mTextPaint, mDisplaySize.getWidth(), Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
 
