67a9327aefa97e6c1b63b87eaa3b1c6656428e24
wing <zlcnkkm@21cn.com>
2015-11-17 09:08:23 +0800

    增加取得系统版本同App版本方法

--- wear/src/main/java/com/magicinstall/wearable/InformationsDrawer.java
+++ wear/src/main/java/com/magicinstall/wearable/InformationsDrawer.java
@@ -1,15 +1,20 @@
 package com.magicinstall.wearable;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.hardware.Sensor;
+import android.os.Build;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.util.Log;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -18,7 +23,9 @@ import java.util.Locale;
 /**
  * Created by wing on 15/11/7.
  */
-public class InformationsDrawer extends WatchFaceDrawer implements SensorsEventCallback{
+public class InformationsDrawer extends WatchFaceDrawer{
+    private String mSystemVersionString;
+    private String mAppVersionString;
 //    private String mDrawString;
     private TextPaint mTextPaint;
     private StaticLayout mTextLayout;
@@ -33,24 +40,30 @@ public class InformationsDrawer extends WatchFaceDrawer implements SensorsEventC
 
 //    private Size mDisplaySize;
 
-    private String mSensorInfoString;
 
     /**
      * 蓝牙状态对象
      */
     BluetoothStatus mBluetoothStatus;
     private String mPhoneConnect = "Phone: Disconnected ";
-    private String mPhoneBattery = "battery:---\n";
+    private String mPhoneBattery = "bat.---\n";
 
+
+    /**
+     * 传感器对象
+     */
+    private SensorMonitor mSensorMonitor;
+    private String mSensorInfoString;
+    private String mHeartString = "HeartRate:---";
 
 
     public InformationsDrawer(CanvasWatchFaceService.Engine engine, Resources resources, Context context) {
         super(engine, resources, context);
         // 指定系统字体
-//        Typeface mFace = Typeface.create("Roboto", Typeface.NORMAL);
+        Typeface mFace = Typeface.create("Roboto", Typeface.NORMAL);
 
         // 指定assets 目录入边嘅TTF字体
-        Typeface mFace = Typeface.createFromAsset(mResources.getAssets(), "fonts/Roboto-Thin.ttf");
+//        Typeface mFace = Typeface.createFromAsset(mResources.getAssets(), "fonts/Roboto-Thin.ttf");
 
         // 设置字体画笔
         mTextPaint = new TextPaint();
@@ -66,11 +79,18 @@ public class InformationsDrawer extends WatchFaceDrawer implements SensorsEventC
 //        mDateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL, Locale.getDefault());
         mDateFormat = new SimpleDateFormat("yyyy/MMMM/d EEEE\nH:m:s z", Locale.getDefault());
 
-        mSensorInfoString = mHardwear.toString();
-        mHardwear.setSensorEventReceiver(this);
-//        mHardwear.ActivateAccelerometerSensor();
+        // 激活传感器
+        mSensorMonitor = newSensorsMonitor(context);
+//        Log.i("InfoDrawer", mSensorMonitor.toString());
+//        mSensorInfoString = mSensorMonitor.toString();
+//        mSensorMonitor.ActivateAccelerometerSensor();
 
+
+        // 蓝牙启动BLE 连接
         activateBluetooth(context);
+
+        mAppVersionString = getAppVersionName(context);
+        mSystemVersionString = getSystemVersionName();
     }
 
     /*
@@ -80,7 +100,40 @@ public class InformationsDrawer extends WatchFaceDrawer implements SensorsEventC
     }
     */
 
-    // 重新组成字串, 新建一个居中Layout
+    /**
+     * 返回当前程序版本名
+     */
+    public static String getAppVersionName(Context context) {
+        String versionName = null;
+        int versioncode = -1;
+        try {
+            // ---get the package info---
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            versionName = pi.versionName;
+            versioncode = pi.versionCode;
+            if (versionName == null || versionName.length() <= 0) {
+                return "";
+            }
+        } catch (Exception e) {
+            Log.e("getAppVersionName", "Exception", e);
+        }
+        return versionName;
+    }
+
+    /**
+     * 返回当前程序版本名
+     */
+    public static String getSystemVersionName() {
+        return /*"Product Model: " +*/ android.os.Build.MODEL + ","
+                + Build.VERSION.SDK_INT + ","
+                + android.os.Build.VERSION.RELEASE ;
+    }
+
+
+    /**
+     * 重新组成字串, 新建一个居中Layout
+     */
     protected final void rebuildTextLayout(){
         // 可见度
 //        String str_visible = (getIsVisible() ? "Visible" : "Invisible") + "\n";
@@ -97,40 +150,48 @@ public class InformationsDrawer extends WatchFaceDrawer implements SensorsEventC
 
         mTextLayout = new StaticLayout(
 //                str_visible +
-                    str_ambient +
-                    mWidthPixels + "x" + mHeightPixels + str_FPS +
-                    mPhoneConnect + mPhoneBattery +
-                    mSensorInfoString +
-                    mDateString,
-                    mTextPaint, mWidthPixels, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false
+                str_ambient
+                        +
+                        mWidthPixels + "x" + mHeightPixels + str_FPS +
+                        mPhoneConnect + mPhoneBattery +
+                        mHeartString +
+                        mDateString +
+                        "Ver. " + mAppVersionString
+                ,
+                mTextPaint, mWidthPixels, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false
         );
 
     }
 
     @Override
     public void Draw(Canvas canvas, Rect bounds) {
-        mDateString = mDateFormat.format(System.currentTimeMillis());
+        mDateString = mDateFormat.format(System.currentTimeMillis()) + "\n";
         rebuildTextLayout();
         mTextLayout.draw(canvas);
     }
 
+    /**
+     * 交互模式切换事件
+     * @param inInteractiveMode
+     */
     @Override
     protected void onInteractiveModeChanged(boolean inInteractiveMode) {
 //        super.onInteractiveModeChanged(inInteractiveMode);
         if (inInteractiveMode) {
-//            mHardwear.ActivateSensorsWithType(new int[]{
-//                            SensorMonitor.TYPE_ACCELEROMETER
-//                    }
-//            );
+            mSensorMonitor.ActivateSensorsWithType(new int[]{
+//                            Sensor.TYPE_ACCELEROMETER,
+                            Sensor.TYPE_HEART_RATE
+                    }
+            );
         }
-        else mHardwear.DeactivateSensors();
+//        else mSensorMonitor.DeactivateSensors();
     }
 
     /**
      * 激活BLE 连接取得电量
      * @param context
      */
-    protected void activateBluetooth(Context context) {
+    private void activateBluetooth(Context context) {
         mBluetoothStatus = new BluetoothStatus(context) {
             /**
              * 手机端电量变化事件
@@ -140,7 +201,7 @@ public class InformationsDrawer extends WatchFaceDrawer implements SensorsEventC
             public void onBatteryLevelChanged(int batteryLevel) {
                 super.onBatteryLevelChanged(batteryLevel);
 //                Log.i("SensorMonitor", "onBatteryLevelChanged:" + batteryLevel + "%");
-                mPhoneBattery = "battery:" + batteryLevel + "%\n";
+                mPhoneBattery = "bat." + batteryLevel + "%\n";
             }
 
             /**
@@ -153,17 +214,35 @@ public class InformationsDrawer extends WatchFaceDrawer implements SensorsEventC
                 super.onConnectStatusChanged(isConnnected);
 
                 mPhoneConnect = "Phone: "+ (isConnnected ? "Connected " : "Disconnected ");
-                if (!isConnnected) mPhoneBattery = "battery:---\n";
+                if (!isConnnected) mPhoneBattery = "bat.---\n";
             }
         };
     }
 
     /**
-     * 加速度传感器值改变事件.
+     * 新建一个匿名继承SensorsMonitor 的实例, 方便写事件代码.
+     * @param context
+     * @return
      */
-    @Override
-    public void onAccelerationChanged(float x, float y, float z) {
-//        System.out.println("needPostInvalidate");
-//        mCallbakeReceiver.needPostInvalidate();
+    private SensorMonitor newSensorsMonitor(Context context) {
+        return new SensorMonitor(context) {
+            /**
+             * 1. 加速度传感器值改变事件.
+             */
+            @Override
+            public void onAccelerometerChanged(float x, float y, float z) {
+                super.onAccelerometerChanged(x, y, z);
+
+            }
+
+            /**
+             * 21. 心率传感器值改变事件.
+             */
+            @Override
+            public void onHeartRateChanged(float rate) {
+                super.onHeartRateChanged(rate);
+                mHeartString = "HeartRate:" + rate + "/Minute\n";
+            }
+        };
     }
 }
